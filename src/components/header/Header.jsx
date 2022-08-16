07e3ba8f5690bb2b5f28b511ae19c8bfa4a3123a
ISA MOHAMMAD ISMAIL <fahimismail75.scc@gmail.com>
2022-08-16 21:33:24 +0600

    date

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,33 @@
 import './header.css'
 import {faBed, faCalendar, faCalendarDays, faCar, faPerson, faPlane, faTaxi } from "@fortawesome/free-solid-svg-icons" 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { DateRange } from 'react-date-range'
+import { useState } from 'react'
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { format } from 'date-fns'
 
 const Header = () => {
+
+  const [open, setOpen] = useState(false)
+
+  const [openOption, setOpenOption] = useState(false)
+
+  const [openRoom, setOpenRoom] = useState(false)
+
+  const [options, setOptions] = useState ({
+    adult: 1,
+    children: 0,
+    room: 1
+  })
+
+  const [date, setDate] = useState ([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: 'selection'
+    }
+  ])
   return (
     <div className='header'>
       <div className="headerContainer">
@@ -43,11 +68,47 @@ const Header = () => {
           </div>
           <div className="headerSearchItem">
             <FontAwesomeIcon icon={faCalendarDays} className='headerIcon' />
-            <span className='headerSearchText'>date to date</span>
+            <span onClick={()=>setOpen(!open)} className='headerSearchText'>{`${format(date[0].startDate, "MM/dd/yy")} to ${format(date[0].endDate, "MM/dd/yy")}`}</span>
+            {open&&<DateRange
+              className='date'
+              editableDateInputs={true}
+              onChange={item => setDate([item.selection])}
+              moveRangeOnFirstSelection={false}
+              ranges={date}
+            />}
           </div>
           <div className="headerSearchItem">
             <FontAwesomeIcon icon={faPerson} className='headerIcon' />
-            <span className='headerSearchText'>2 adullts 2 children 1 room</span>
+            <span onClick={()=> setOpenRoom(!openRoom)} className='headerSearchText'>{`${options.adult} adult . ${options.children} children . ${options.room} room`}</span>
+            {openRoom&&<div className="options">
+              <div className="optionItem">
+                <span className="optionText">Adult</span>
+                <div className="optionCounter">
+                  <button onClick={()=> setOptions({...options, adult: options.adult-1})} className="optionCounterButton">-</button>
+                  <span className="optionCounterNumber">{options.adult}</span>
+                  <button onClick={()=> setOptions({...options, adult: options.adult+1})} className="optionCounterButton">+</button>
+                </div>
+              </div>
+              <div className="optionItem">
+                <span className="optionText">Children</span>
+                <div className="optionCounter">
+                  <button onClick={()=> setOptions({...options, children: options.children-1})} className="optionCounterButton">-</button>
+                  <span className="optionCounterNumber">{options.children}</span>
+                  <button onClick={()=> setOptions({...options, children: options.children+1})} className="optionCounterButton">+</button>
+                </div>
+              </div>
+              <div className="optionItem">
+                <span className="optionText">Room</span>
+                <div className="optionCounter">
+                  <button onClick={()=> setOptions({...options, room: options.room-1})} className="optionCounterButton">-</button>
+                  <span className="optionCounterNumber">{options.room}</span>
+                  <button onClick={()=> setOptions({...options, room: options.room+1})} className="optionCounterButton">+</button>
+                </div>
+              </div>
+            </div>}
+          </div>
+          <div className="headerSearchItem">
+            <button className="headerBtn">Search</button>
           </div>
         </div>
       </div>
